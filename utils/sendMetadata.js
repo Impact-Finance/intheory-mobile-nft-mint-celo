f5bf6fb278d78e76c789a1c588b5b9992b01ae5d
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2022-10-13 18:04:56 -0600

    Update sendMetadata.js

--- utils/sendMetadata.js
+++ utils/sendMetadata.js
@@ -7,20 +7,20 @@ export default async function sendMetadata(topics, imgURL) {
   //   const image = await objURL.decode();
   console.log(image);
 
-  const nft = {
-    image,
-    name: 'inTheory pre-launch NFT',
-    description:
-      'The holder of this NFT has demonstrated their commitment to open access science.',
-    properties: {
-      topics,
-      author: 'Impact Finance',
-    },
-  };
-  const client = new NFTStorage({ token: API_KEY });
-  const metadata = await client.store(nft);
+  //   const nft = {
+  //     image,
+  //     name: 'inTheory pre-launch NFT',
+  //     description:
+  //       'The holder of this NFT has demonstrated their commitment to open access science.',
+  //     properties: {
+  //       topics,
+  //       author: 'Impact Finance',
+  //     },
+  //   };
+  //   const client = new NFTStorage({ token: API_KEY });
+  //   const metadata = await client.store(nft);
 
-  console.log('NFT data stored!');
-  console.log('Metadata URI: ', metadata.url);
+  //   console.log('NFT data stored!');
+  //   console.log('Metadata URI: ', metadata.url);
   //   URL.revokeObjectURL(objURL);
 }
