33bed43a4ea178f6ff6ba73b0f0044599c3797f3
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2022-12-26 11:37:59 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,18 @@ export default function Home() {
           The minting period has{' '}
           <strong style={{ color: '#fe00fe' }}>closed</strong>.
         </p>
+        <p>
+          <strong>6,961</strong> inTheory NFTs minted
+        </p>
+        <p>
+          <a
+            style={{ filter: 'opacity(0.7)', textDecoration: 'underline' }}
+            href="https://explorer.celo.org/mainnet/token/0xFF5e58Ce9408858f6BB5Fb979b08E9D6f21A276d/inventory"
+            target="_blank"
+            rel="noopener noreferrer">
+            View on Explorer →
+          </a>
+        </p>
       </div>
       <div>
         <Link href="/how-it-works">
