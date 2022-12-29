7a4f22ff25551fe6a74c752b0fbe42d6a4c53902
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2022-12-29 09:35:19 -0700

    closed-mint

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,4 @@
-import styles from '../styles/Home.module.css';
-import GlobalContext from '../utils/global-context';
-import Link from 'next/link';
-import { useEffect, useContext } from 'react';
-
 export default function Home() {
-  const global = useContext(GlobalContext);
-  useEffect(() => {
-    global.update({
-      ...global,
-      submittedTopics: [],
-      imageURL: '',
-      txnID: '',
-      metadataCID: '',
-    });
-  }, []);
-
   return (
     <>
       <div>
@@ -24,16 +8,20 @@ export default function Home() {
           NFT Mint
         </h1>
         <p style={{ fontSize: '1.4em' }}>
-          Mint your <strong>free</strong> research-inspired NFT artwork to
-          unlock exclusive benefits when inTheory launches in 2023.
+          The minting period has{' '}
+          <strong style={{ color: '#68eaff' }}>closed</strong>.
         </p>
-      </div>
-      <div>
-        <Link href="/how-it-works">
-          <button className="gradient-border main-button">Start →</button>
-        </Link>
-        <p style={{ filter: 'opacity(0.7)' }}>
-          We&apos;ll even cover the gas fees.
+        <p>
+          <strong>6,961</strong> inTheory NFTs minted
+        </p>
+        <p>
+          <a
+            style={{ filter: 'opacity(0.7)', textDecoration: 'underline' }}
+            href="https://explorer.celo.org/mainnet/token/0xFF5e58Ce9408858f6BB5Fb979b08E9D6f21A276d/inventory"
+            target="_blank"
+            rel="noopener noreferrer">
+            View on Explorer →
+          </a>
         </p>
       </div>
     </>
